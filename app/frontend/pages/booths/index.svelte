279e02a139c1ea4343dbc48a1128881fcfb67c6d
Stefan Buhrmester <buhrmi@gmail.com>
2022-09-23 14:27:10 +0200

    tweet

--- app/frontend/pages/booths/index.svelte
+++ app/frontend/pages/booths/index.svelte
@@ -4,9 +4,5 @@
 
 
 <div class="container">
-  Open DJ Booth is a free, open source project in the spirit of <a href="https://plug.dj">plug.dj</a>.
-  It's currently in development. Nothing works. Please check back later. If you're interested in development progress,
-  please <a href="https://github.com/buhrmi/opendjbooth">check out the GitHub repo</a>.
-  <br>
-  <a href="/booths/new" use:inertia class="btn primary">Create a DJ Booth</a>
+  <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Open DJ Booth is an open-source project in the spirit of <a href="https://twitter.com/plugdj?ref_src=twsrc%5Etfw">@plugdj</a>. It is currently in development. Nothing works. Please check back later.<br>If you&#39;re interested in development progress, please check out our GitHub repo: <a href="https://t.co/mhwWogGh1E">https://t.co/mhwWogGh1E</a></p>&mdash; Open DJ Booth (@opendjbooth) <a href="https://twitter.com/opendjbooth/status/1573287421600464897?ref_src=twsrc%5Etfw">September 23, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
