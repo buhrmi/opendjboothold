b672cf4ae15b38af865e392c56810ac5d0e6bb47
Stefan Buhrmester <buhrmi@gmail.com>
2022-09-23 14:19:22 +0200

    repo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Open DJ Booth
 
 An open-source project to carry on and honor the spirit of [plug.dj](https://plug.dj)
+
+### Support
+
+Please [open a new issue](https://github.com/buhrmi/opendjbooth/issues/new) if you have any questions or problems.
--- app/frontend/pages/booths/index.svelte
+++ app/frontend/pages/booths/index.svelte
@@ -4,8 +4,9 @@
 
 
 <div class="container">
-  Open DJ Booth is a free, <a href="https://github.com/buhrmi/opendjbooth">open source</a> project in the spirit of <a href="https://plug.dj">plug.dj</a>.
-  It's currently in development. Nothing works. Please check back later.
+  Open DJ Booth is a free, open source project in the spirit of <a href="https://plug.dj">plug.dj</a>.
+  It's currently in development. Nothing works. Please check back later. If you're interested in development progress,
+  please <a href="https://github.com/buhrmi/opendjbooth">check out the GitHub repo</a>.
   <br>
   <a href="/booths/new" use:inertia class="btn primary">Create a DJ Booth</a>
 </div>
