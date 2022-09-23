1c379573e1ff5e41c8f9ce287c6b834b9a8f616f
Stefan Buhrmester <buhrmi@gmail.com>
2022-09-23 15:08:57 +0200

    raise when email is empty

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -7,8 +7,8 @@ class Identity < ApplicationRecord
   validates :provider_id, presence: true, uniqueness: { scope: :provider }
 
   def self.from_omniauth! auth_hash
-    identity = Identity.find_or_create_by(provider: auth_hash['provider'], provider_id: auth_hash['uid']) do |identity|
-      identity.user ||= User.new(email: auth_hash['info']['email'], password: SecureRandom.hex, display_name: auth_hash['info']['name'], name: auth_hash['info']['nickname'])
+    identity = Identity.where(provider: auth_hash['provider'], provider_id: auth_hash['uid']).first_or_create! do |identity|
+      identity.user ||= User.where(email: auth_hash['info']['email']).first_or_create!(password: SecureRandom.hex, display_name: auth_hash['info']['name'], name: auth_hash['info']['nickname'])
       if auth_hash['provider'] == 'twitter'
         image = URI.parse(auth_hash['info']['image']).open
         identity.user.avatar.attach(io: image, filename: 'avatar.jpg')
