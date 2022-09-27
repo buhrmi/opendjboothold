ca6259099fa1af6e39c9013d1f2e994201f761e5
Stefan Buhrmester <buhrmi@gmail.com>
2022-09-27 18:21:07 +0200

    fix test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -5,6 +5,7 @@ class UsersTest < ApplicationSystemTestCase
   test 'can log in and out' do
     visit '/'
     click_on 'Log in'
+    click_on 'Twitter'
     assert_text 'Log out'
     click_on 'Log out'
     assert_text 'Log in'
